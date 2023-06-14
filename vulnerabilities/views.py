65bb35befa41ec94f70c6bb853a9827649f1987e
Mayersi Lorena Parra Guerrero <lorena.parra@FZCORP.local>
2023-06-14 16:24:50 -0500

    signup app

---
+++ vulnerabilities/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        print( UserCreationForm)
+        return render(request, 'signup.html', {"form": UserCreationForm})
+    else:
+
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect('vulnerabilities')
+            except IntegrityError:
+                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
+
+        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
+
+
+def vulnerabilities(request):
+    return render(request, 'vulnerabilities.html')
